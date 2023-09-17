e0855340ece9245ee982d17218ecb5f4d3d2d85b
erfanchy93 <erfan@stockwell.ai>
2023-09-17 00:01:35 -0700

    Initial Commit

---
+++ test_path.py
@@ -0,0 +1,7 @@
+import os
+
+path_to_dataset = "/media/erfan/Extreme SSD/rsna-2022-cervical-spine-fracture-detection"
+
+files_in_path_to_dataset = os.listdir(path_to_dataset)
+
+print(files_in_path_to_dataset)
